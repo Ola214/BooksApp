b059b12dfbfd595d6539c187bc24c48ebd989d0e
Ola214 <aleksandra.krysinska.94@gmail.com>
2025-10-15 19:35:27 +0200

    C5 - filter books

--- src/js/script.js
+++ src/js/script.js
@@ -50,6 +50,7 @@
       const thisBooks = this;
       thisBooks.books = books;
       thisBooks.favouriteBooks = [];
+      thisBooks.filters = [];
 
       thisBooks.initActions();
     }
@@ -78,10 +79,70 @@
             this.favouriteBooks.push(bookImage.getAttribute('data-id'));
           }
         }
-      });
-    }
-  }
 
+      });
+
+      const filters = document.querySelectorAll('.filters');
+
+
+      for(let i = 0; i < 2; i++) {
+
+
+        filters[0].children[0].children[i].addEventListener('click', (e) => {
+          this.filters = [];
+          const check = e.target;
+
+
+          if(
+            (check.getAttribute('type') == 'checkbox') &&
+            (check.getAttribute('name') == 'filter')
+          )
+          {
+
+            if(check.checked == 1) {
+              this.filters.push(check.getAttribute('value'));
+            }
+
+            if(check.checked != 1){
+
+              const index = this.filters.indexOf(check.getAttribute('value'));
+
+              if(index !== -1){
+                this.filters.splice(index, 1);
+              }
+            }
+          }
+          this.check = check;
+          this.filterBooks();
+
+
+        });
+      }
+    }
+
+    filterBooks() {
+      const books = this.books; //wszystkie książki
+
+      for(let bookId in books) { //każdy index książki
+        const book = books[bookId]; //pojedyncza książka z kolekcji wszystkich książek
+
+
+        const isBookInThisFilter = book.data.details[this.check.getAttribute('value')]; // czy książka ma filtr taki jak kliknięty w filtrach, nie ważne, czy na true, czy na false
+
+        if(isBookInThisFilter) { // jeżeli książka ma filtr taki jak kliknięty w filtrach, nie ważne ,czy na true, czy na false
+
+          if(book.element.children[1].classList.contains('hidden')) { //jeżeli  ma klasę hidden to usuń ją, będzie niewyszarzone +checkbox
+            book.element.children[1].classList.remove('hidden'); //niewyszarzone +checkbox
+          }
+          else { //jeżeli nie ma klasy hidden to dodaj ją, będzie wyszarzone +checkbox
+            book.element.children[1].classList.add('hidden'); // wyszarzone +checkbox
+          }
+        }
+      }
+
+    }
+
+  }
 
   const app = {
     initMenu: function() {
@@ -111,9 +172,8 @@
     init: function() {
       const thisApp = this;
       thisApp.initData();
-    }
+    },
   };
 
   app.init();
-
 }
